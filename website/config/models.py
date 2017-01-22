0d6a38bf3f04ee5aef63586ea02ec70021c597f0
eduzen <eduardo.a.enriquez@gmail.com>
2017-01-22 18:27:38 -0300

    title

--- website/config/models.py
+++ website/config/models.py
@@ -6,6 +6,7 @@ from solo.models import SingletonModel
 class SiteConfiguration(SingletonModel):
     title = models.CharField(max_length=255, default='Site Name')
     subtitle = models.CharField(max_length=255, default='Subtitle Name')
+    tab_title = models.CharField(max_length=255, default='Tab title')
     home = models.URLField(max_length=500, default='eduardoenriquez.com.ar')
     url_linkedin = models.URLField(max_length=500, default='linkedin')
     url_github = models.URLField(max_length=500, default='github')
