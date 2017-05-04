cecf68d5eaca732a1f96f6d1f760cbf519ddb3c0
eduzen <eduardo.a.enriquez@gmail.com>
2017-05-03 23:34:06 -0300

    twitter solo config

--- website/blog/views.py
+++ website/blog/views.py
@@ -10,18 +10,21 @@ from django.shortcuts import redirect
 from .models import Post, Comment
 from .models import CustomPage
 from .forms import CommentForm
+from config.models import TwitterConfiguration
 
 
 def get_tweets():
     """
     returns twitter feed with settings as described below, contains all related twitter settings
     """
-    api = twitter.Api(consumer_key='yourcustomerkey',
-                      consumer_secret='customerkeysecret',
-                      access_token_key='accesstokenkey',
-                      access_token_secret='accesstokensecret')
+    twitter_conf = TwitterConfiguration.objects.get()
+    api = twitter.Api(twitter_conf)
 
-    return api.GetUserTimeline(screen_name='twitter_screen_name', exclude_replies=True, include_rts=False)  # includes entities
+    return api.GetUserTimeline(
+        screen_name='twitter_screen_name',
+        exclude_replies=True,
+        include_rts=False)  # includes entities
+
 
 def home(request):
     posts = Post.objects.filter(
