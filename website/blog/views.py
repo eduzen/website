707fcb14c7f36e7c0932e84f927523dd2e04df7a
eduzen <eduardo.a.enriquez@gmail.com>
2017-05-06 15:50:45 -0300

    more changes on design of templates

--- website/blog/views.py
+++ website/blog/views.py
@@ -1,5 +1,4 @@
 # -*- coding: utf-8 -*-
-import twitter
 from django.contrib.auth.decorators import login_required
 
 from django.shortcuts import render
@@ -10,21 +9,6 @@ from django.shortcuts import redirect
 from .models import Post, Comment
 from .models import CustomPage
 from .forms import CommentForm
-from config.models import TwitterConfiguration
-
-
-def get_tweets():
-    """
-    returns twitter feed with settings as described below, contains all related twitter settings
-    """
-    twitter_conf = TwitterConfiguration.objects.get()
-    api = twitter.Api(twitter_conf)
-
-    return api.GetUserTimeline(
-        screen_name='twitter_screen_name',
-        exclude_replies=True,
-        include_rts=False)  # includes entities
-
 
 def home(request):
     posts = Post.objects.filter(
