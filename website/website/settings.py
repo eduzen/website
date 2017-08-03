8326cb494542529f39d6209805bd258c4066f2f8
eduzen <eduardo.a.enriquez@gmail.com>
2017-08-02 23:08:45 -0300

    fix

--- website/website/settings.py
+++ website/website/settings.py
@@ -18,8 +18,13 @@ try:
         ANYMAIL, EMAIL_BACKEND, DEFAULT_FROM_EMAIL,
     )
 except Exception:
-    pass
-
+    SECRET_KEYSECRET_KEY = "9-t+&@+bo$l263c!s_3vobwh0_1bx^m93wih^-s+bt1xljfrnv"
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': 'mydatabase',
+        }
+    }
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
