fa84a339fff1ed9341a1d5b451f9808f1eff2974
eduzen <eduardo.a.enriquez@gmail.com>
2017-10-28 16:59:15 -0300

    updated tests

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 [![Build Status](https://travis-ci.org/eduzen/eduzen.svg?branch=master)](https://travis-ci.org/eduzen/eduzen)
+[![codecov](https://codecov.io/gh/eduzen/eduzen/branch/master/graph/badge.svg)](https://codecov.io/gh/eduzen/eduzen)
+
 
 # My django blog Repo: http://eduzen.com.ar
 
--- website/tests/blog/models/test_post_model.py
+++ website/tests/blog/models/test_post_model.py
@@ -13,6 +13,15 @@ def superuser():
     return user
 
 
+@pytest.fixture()
+def post():
+    user = User.objects.create_superuser('myuser', 'myemail@test.com', 'pswd')
+    post = Post.objects.create(
+        title='My post1', slug='my-post1', text='Lorem ipsum1', author=user,
+        published_date=datetime.now(), created_date=datetime.now())
+    return post
+
+
 @pytest.mark.django_db
 def test_create_post(superuser):
     post = Post.objects.create(
@@ -21,3 +30,13 @@ def test_create_post(superuser):
 
     post_expected = Post.objects.get()
     assert post_expected
+
+
+@pytest.mark.django_db
+def test_modified_post(post):
+    post = Post.objects.get(post.id)
+    post.text = 'other text'
+    post.save()
+
+    post_expected = Post.objects.get(post.id)
+    assert post_expected.text == 'other text'
