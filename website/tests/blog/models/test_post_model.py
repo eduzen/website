302d52a01f561f5c8592ac4e43896fd0ec47c9fd
eduzen <eduardo.a.enriquez@gmail.com>
2017-10-28 17:20:46 -0300

    updated tests and req

--- website/tests/blog/models/test_post_model.py
+++ website/tests/blog/models/test_post_model.py
@@ -14,11 +14,13 @@ def superuser():
 
 
 @pytest.fixture()
-def post():
+@pytest.mark.django_db
+def post(mocker):
     user = User.objects.create_superuser('myuser', 'myemail@test.com', 'pswd')
     post = Post.objects.create(
         title='My post1', slug='my-post1', text='Lorem ipsum1', author=user,
         published_date=datetime.now(), created_date=datetime.now())
+    post.save()
     return post
 
 
@@ -34,9 +36,9 @@ def test_create_post(superuser):
 
 @pytest.mark.django_db
 def test_modified_post(post):
-    post = Post.objects.get(post.id)
+    post = Post.objects.filter(id=post.id)[0]
     post.text = 'other text'
     post.save()
 
-    post_expected = Post.objects.get(post.id)
+    post_expected = Post.objects.filter(id=post.id)[0]
     assert post_expected.text == 'other text'
