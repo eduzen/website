67bb237760efd93e8a413707ae391e0f9500a5c6
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2019-04-27 20:06:08 +0200

    improve script

--- scripts/gunicorn_start.sh
+++ scripts/gunicorn_start.sh
@@ -7,7 +7,25 @@ DJANGO_WSGI_MODULE=website.wsgi      # WSGI module name
 BIND=0.0.0.0:8080
 TIMEOUT=120
 
-echo "Starting $NAME as `whoami`"
+function postgres_ready(){
+python << END
+import sys
+import psycopg2
+try:
+    conn = psycopg2.connect(dbname="$DB_NAME", user="$DB_USER", password="$DB_PASS", host="$DB_SERVICE")
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
+
+until postgres_ready; do
+  >&2 echo "Postgres is unavailable - sleeping"
+  sleep 1
+done
+>&2 echo "Postgres is up - continuing..."
+
+echo "### Starting $NAME as `whoami` with $DJANGO_SETTINGS_MODULE"
 
 # Start your Django Unicorn
 # Programs meant to be run under supervisor should not daemonize themselves (do not use --daemon)
