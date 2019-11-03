f681523f4b82dbba7ded10c2568c14832725bb82
Eduardo Enriquez (eduzen) <eduardo.enriquez@teleclinic.com>
2019-11-03 17:31:05 +0100

    Flake8

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,4 @@
 import logging
-import requests
 from collections import defaultdict
 from django.shortcuts import render, redirect, get_object_or_404, get_list_or_404
 from django.contrib.postgres.search import SearchVector
