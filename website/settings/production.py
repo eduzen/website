5da53f5fb7d69cb4bc2a9d80e02769f3a3e4c1b2
Eduardo Enriquez (eduzen) <eduardo.enriquez@teleclinic.com>
2020-02-17 21:23:18 +0100

    Updating settings

--- website/settings/production.py
+++ website/settings/production.py
@@ -27,11 +27,11 @@ MEDIA_URL = "https://media.eduzen.com.ar/"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": os.environ.get("DB_NAME"),
-        "USER": os.environ.get("DB_USER"),
-        "PASSWORD": os.environ.get("DB_PASS"),
-        "HOST": os.environ.get("DB_SERVICE"),
-        "PORT": os.environ.get("DB_PORT"),
+        "NAME": os.getenv("DB_NAME"),
+        "USER": os.getenv("DB_USER"),
+        "PASSWORD": os.getenv("DB_PASS"),
+        "HOST": os.getenv("DB_SERVICE"),
+        "PORT": os.getenv("DB_PORT"),
     }
 }
 
