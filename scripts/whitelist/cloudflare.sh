07dc7c1f2c4da5a62ab907c0dc0e8ed23b6399e3
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2020-07-25 12:27:48 +0200

    Update

--- scripts/whitelist/cloudflare.sh
+++ scripts/whitelist/cloudflare.sh
@@ -3,5 +3,5 @@ cd /tmp
 wget https://www.cloudflare.com/ips-v4 -O ips-v4-$$.tmp
 wget https://www.cloudflare.com/ips-v6 -O ips-v6-$$.tmp
 
-for cfip in `cat ips-v4-$$.tmp`; do echo "ufw allow from $cfip to any port 443 proto tcp"; done
-for cfip in `cat ips-v6-$$.tmp`; do echo "ufw allow from $cfip to any port 443 proto tcp"; done
+for cfip in `cat ips-v4-$$.tmp`; do ufw allow from $cfip to any port 80 proto tcp; done
+for cfip in `cat ips-v6-$$.tmp`; do ufw allow from $cfip to any port 80 proto tcp; done
