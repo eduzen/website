a2fd0c100c376bda806280463a7bf89f49de9aa1
eduzen <eduardo.a.enriquez@gmail.com>
2020-10-25 15:26:36 +0100

    🚀 Moving to hetzner

---
+++ website/settings/dev.py
@@ -0,0 +1,40 @@
+import os
+
+from configurations import values
+
+from .base import Base, StaticMedia
+
+
+class Dev(StaticMedia, Base):
+    DEBUG = True
+    SECRET_KEY = "s0m3r4nd0mk3yford3v!"
+    ALLOWED_HOSTS = values.ListValue(["*"])
+
+    EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+    EMAIL_HOST = "mailhog"  # Your Mailhog Host
+    EMAIL_PORT = "1025"
+
+    DATABASES = values.DatabaseURLValue(conn_max_age=600, ssl_require=False)
+
+    ANYMAIL = {
+        "MAILGUN_API_KEY": os.environ.get("MAILGUN_API_KEY"),
+        "MAILGUN_SENDER_DOMAIN": os.environ.get("MAILGUN_SENDER_DOMAIN"),
+    }
+
+    SHELL_PLUS_PRINT_SQL = True
+    SHELL_PLUS_PRINT_SQL_TRUNCATE = None
+    INTERNAL_IPS = ["127.0.0.1"]
+
+    CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}}
+
+    @property
+    def DEBUG_TOOLBAR_CONFIG(self):
+        return {"SHOW_TOOLBAR_CALLBACK": lambda x: True}
+
+    @property
+    def INSTALLED_APPS(self):
+        return super().INSTALLED_APPS + ["debug_toolbar"]
+
+    @property
+    def MIDDLEWARE(self):
+        return ["debug_toolbar.middleware.DebugToolbarMiddleware"] + super().MIDDLEWARE
