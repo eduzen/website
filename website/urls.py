b806e274e443e85bcb484f14fc015c86ab379a06
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2023-03-01 19:13:59 +0100

    :art: Debugging

--- website/urls.py
+++ website/urls.py
@@ -9,7 +9,7 @@ from .sitemap import sitemaps
 from .views import MediaView, favicon_view
 
 urlpatterns = [
-    path("", include("snippets.urls")),
+    path("api/snippets/", include("snippets.urls")),
     path("api/", include("api.urls")),
     path("healthchecks/", include("django_healthchecks.urls")),
     path("media/<path>", MediaView.as_view()),
