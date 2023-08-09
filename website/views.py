fd14fd5f9d10674228c29da2956b163230a2e9ba
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2023-08-09 16:56:55 +0200

    wip

--- website/views.py
+++ website/views.py
@@ -1,9 +1,5 @@
 import logging
 
-from django.conf import settings
-from django.http import HttpRequest, HttpResponse
-from django.shortcuts import redirect
-from django.utils import translation
 from django.views.generic.base import RedirectView
 
 logger = logging.getLogger("website.views")
@@ -27,10 +23,3 @@ class StaticView(RedirectView):
         self.url = f"https://static.eduzen.com.ar/{kwargs['path']}"
         logger.warn("url redirected %s", (self.url,))
         return super().get_redirect_url(*args, **kwargs)
-
-
-def set_language(request: HttpRequest, language_code: str) -> HttpResponse:
-    if translation.check_for_language(language_code):
-        translation.activate(language_code)
-        request.session[settings.LANGUAGE_COOKIE_NAME] = language_code
-    return redirect(request.headers.get("referer"))
