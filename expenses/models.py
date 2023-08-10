b3b04a347e8d7a72e6ec25bd9d45170489eb907d
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2023-08-10 09:47:50 +0200

    wip refactor

--- expenses/models.py
+++ expenses/models.py
@@ -3,7 +3,7 @@ from django.utils import timezone
 from djmoney.models.fields import MoneyField
 
 
-class Expense(models.Model):
+class Expense(models.Model):  # type: ignore
     title = models.CharField(max_length=200, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     Category = models.ForeignKey("expenses.Category", blank=True, null=True, on_delete=models.CASCADE)
@@ -16,7 +16,7 @@ class Expense(models.Model):
         return f"<Expense: {self.title} - {self.amount}>"
 
 
-class Category(models.Model):
+class Category(models.Model):  # type: ignore
     name = models.CharField(max_length=200, unique=True)
     created_date = models.DateTimeField(default=timezone.now)
 
