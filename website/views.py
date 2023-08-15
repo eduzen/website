70984992650813654e5f5b5913422c4137879931
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2023-08-15 14:57:03 +0200

    :art: Add favicon

--- website/views.py
+++ website/views.py
@@ -1,10 +1,11 @@
 import logging
 
+from django.http import HttpRequest, HttpResponse
+from django.views.decorators.cache import cache_control
+from django.views.decorators.http import require_GET
 from django.views.generic.base import RedirectView
 
 logger = logging.getLogger("website.views")
-
-favicon_view = RedirectView.as_view(url="https://static.eduzen.com.ar/blog/img/favicon.ico", permanent=True)
 
 
 class MediaView(RedirectView):
@@ -23,3 +24,16 @@ class StaticView(RedirectView):
         self.url = f"https://static.eduzen.com.ar/{kwargs['path']}"
         logger.warn("url redirected %s", (self.url,))
         return super().get_redirect_url(*args, **kwargs)
+
+
+@require_GET
+@cache_control(max_age=60 * 60 * 24 * 365, immutable=True, public=True)  # one year
+def favicon_view(request: HttpRequest) -> HttpResponse:
+    return HttpResponse(
+        (
+            '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 100 100">'
+            + '<text y=".9em" font-size="90">🤓</text>'
+            + "</svg>"
+        ),
+        content_type="image/svg+xml",
+    )
