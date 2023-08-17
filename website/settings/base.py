d1a7b29df113845be2ea7d78b738022890a8da8e
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2023-08-17 21:42:11 +0200

    :art: Remove unused code

--- website/settings/base.py
+++ website/settings/base.py
@@ -101,6 +101,9 @@ class WhitenoiseStatic:
     STATIC_URL = "/static/"
     STATIC_ROOT = BASE_DIR / "assets"
     STORAGES = {
+        "default": {
+            "BACKEND": "django.core.files.storage.FileSystemStorage",
+        },
         "staticfiles": {
             "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
         },
