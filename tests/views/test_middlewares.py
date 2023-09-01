1fd92143fa59986e2bf264311031e3aaf5fe1b9e
Eduardo Enriquez (eduzen) <eduardo.a.enriquez@gmail.com>
2023-09-01 17:09:52 +0200

    A fix for middleware

---
+++ tests/views/test_middlewares.py
@@ -0,0 +1,24 @@
+from unittest import mock
+from django.test import TestCase
+from website.middleware import CloudflareMiddleware
+from django.test.client import RequestFactory
+
+
+class TestCloudflareMiddleware(TestCase):
+    def setUp(self) -> None:
+        self.get_response = mock.MagicMock()
+        self.middleware = CloudflareMiddleware(self.get_response)
+        super().setUp()
+
+    def test_requestProcessing(self):
+        """Test that the client IP is correctly set from CF-Connecting-IP."""
+        headers = {"CF-Connecting-IP": "123.123.123.123"}
+        request = RequestFactory(headers=headers).get("/")
+        self.middleware(request)
+        assert request.ip == "123.123.123.123"
+
+    def test_fallback_to_remote_addr(self):
+        """Test that the client IP falls back to REMOTE_ADDR if CF-Connecting-IP is not present."""
+        request = RequestFactory().get("/")
+        self.middleware(request)
+        assert request.ip == "127.0.0.1"
--- website/middleware.py
+++ website/middleware.py
@@ -1,12 +1,36 @@
+from collections.abc import Callable
+from django.http import HttpRequest, HttpResponse
+
+
 class CloudflareMiddleware:
-    def __init__(self, get_response):
+    """
+    Middleware for correctly obtaining the client's IP address when the
+    Django application is behind Cloudflare's proxy. When the application
+    is behind Cloudflare's reverse proxy, the actual IP address of the client
+    is stored in the 'CF-Connecting-IP' header. This middleware ensures that
+    the application gets the correct client IP, either from the header added
+    by Cloudflare or falls back to the default Django method.
+    """
+
+    def __init__(self, get_response: Callable) -> None:
+        """
+        Initialize the middleware with the given get_response callable.
+
+        Args:
+            get_response (callable): The next middleware or view to process the request.
+        """
         self.get_response = get_response
 
-    def __call__(self, request):
-        try:
-            ip = request.headers["CF-Connecting-IP"]
-        except KeyError:
-            ip = request.META["REMOTE_ADDR"]
-        request.ip = ip
+    def __call__(self, request: HttpRequest) -> HttpResponse:
+        """
+        Process the request to set the correct client IP address.
 
+        Args:
+            request (HttpRequest): The incoming request object.
+
+        Returns:
+            HttpResponse: The response object after processing.
+        """
+        ip = request.headers.get("CF-Connecting-IP", request.META["REMOTE_ADDR"])
+        request.ip = ip
         return self.get_response(request)
