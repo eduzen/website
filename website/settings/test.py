557734dc422b0db587e218bf375f14ddad2d424d
Eduardo Enriquez <eduardo.a.enriquez@gmail.com>
2024-12-28 14:01:10 +0100

    Update Docker configuration and Justfile; streamline environment … (#622)
    * Update Docker configuration and Justfile; streamline environment setup and modify project dependencies
    
    * admin without translations

--- website/settings/test.py
+++ website/settings/test.py
@@ -1,28 +1,20 @@
-from .base import *
-from .base import STORAGES  # noqa
+from .base import *  # noqa
 
 DEBUG = False
 TEMPLATE_DEBUG = False
 DEBUG_LOGGING = False
 
-PASSWORD_HASHERS = [
-    "django.contrib.auth.hashers.MD5PasswordHasher",
-]
+PASSWORD_HASHERS = ["django.contrib.auth.hashers.MD5PasswordHasher"]
 
 EMAIL_BACKEND = "django.core.mail.backends.locmem.EmailBackend"
 
 CACHES = {"default": {"BACKEND": "django.core.cache.backends.dummy.DummyCache"}}
 
-STORAGES["default"]["backend"] = "inmemorystorage.InMemoryStorage"
-
-MIDDLEWARE = [
-    "django.middleware.security.SecurityMiddleware",
-    "django.contrib.sessions.middleware.SessionMiddleware",
-    "corsheaders.middleware.CorsMiddleware",
-    "django_htmx.middleware.HtmxMiddleware",
-    "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
-    "django.contrib.auth.middleware.AuthenticationMiddleware",
-    "django.contrib.messages.middleware.MessageMiddleware",
-    "django.middleware.clickjacking.XFrameOptionsMiddleware",
-]
+STORAGES = {
+    "default": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage"
+    },
+    "staticfiles": {
+        "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+    },
+}
