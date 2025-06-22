005cd0ff67de72a4b3889504b9e23f07dfc91725
Eduardo Enriquez <eduardo.a.enriquez@gmail.com>
2025-06-22 14:32:33 +0200

    test: expand email form assertions (#648)

--- blog/tests/forms_test.py
+++ blog/tests/forms_test.py
@@ -9,3 +9,7 @@ def test_mail(mailoutbox):
     email_form.send_email()
 
     assert len(mailoutbox) == 1
+    email = mailoutbox[0]
+    assert data["subject"] in email.subject
+    assert data["message"] in email.body
+    assert email.from_email == settings.DEFAULT_FROM_EMAIL
